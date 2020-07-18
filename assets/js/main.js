e0e009fa2ee5bdd28aef805f9c12a8afeb46c5e0
Denis <den.vasilyuk@gmail.com>
2020-07-18 04:37:20 +0300

    ,odals

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,16 @@ function react() {
   }
 }
 
+$('.questions_card').on('click', (e) => {
+  $(e.currentTarget).children('.questions_card-text').toggle('fast')
+  console.log($(e.currentTarget).find('.questions_card-img').css('transform'))
+  if ($(e.currentTarget).find('.questions_card-img').css('transform')!=='none') {
+      $(e.currentTarget).find('.questions_card-img').css('transform', 'none')
+  } else {
+      $(e.currentTarget).find('.questions_card-img').css({
+          'transform': 'rotate(180deg)',
+          'transition': 'all 400ms'
+      })
+  }
+
+})

0de00c1f49d47eebafbf80832db2e25ec2cad76d
Denis <den.vasilyuk@gmail.com>
2020-07-24 01:28:12 +0300

    pravki

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,7 +133,15 @@ $(window).scroll(function(){
 	}
 });
 
-$('a[href^="#"]').click(function () {
+$('.map_item').click(function () {
+  $root.animate({
+    scrollTop: $( $.attr(this, 'href') ).offset().top - 200
+  }, 500);
+
+  return false;
+});
+
+$('.brick_types-btn').click(function () {
   $root.animate({
     scrollTop: $( $.attr(this, 'href') ).offset().top - 200
   }, 500);
@@ -158,3 +166,28 @@ $('form').submit(function(event) {
         }
      })
 });
+
+// функция возвращает cookie с именем name, если есть, если нет, то undefined    
+function getCookie(name) {
+  var matches = document.cookie.match(new RegExp(
+  "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+// проверяем, есть ли у нас cookie, с которой мы не показываем окно и если нет, запускаем показ
+var alertwin = getCookie("alertwin");
+if (alertwin != "no") { 
+  let n = 0;
+  $(document).mouseleave(function(e){
+      if (e.clientY < 10) {
+        if ( n < 2 ) {
+          $('#exit').modal('show'); 
+          n++; 
+        }
+          // записываем cookie на 1 день, с которой мы не показываем окно
+          var date = new Date;
+          date.setDate(date.getDate() + 1);    
+          document.cookie = "alertwin=no; path=/; expires=" + date.toUTCString();       
+      }    
+  });
+}
